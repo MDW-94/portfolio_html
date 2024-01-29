de64150a01b7359e5d387b9f373250ca5f8671c6
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-01-29 15:42:13 +0000

    initial commit test

---
+++ script.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     $("a").on('click', function(event){
+//         if(this.has !== "") {
+//         event.preventDefault();
+//         let hash = this.hash;
+
+//         $('html', 'body').animate({
+//         scrollTop: $(hash).offset().top
+//         }, 900, function(){
+//         window.location.hash = hash;
+//         });
+//     }
+//     });
+// });
